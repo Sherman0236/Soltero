fb2ca6452048ddc7e32a7a7d9dfc35c93d9a2b4a
Sherman#0236 <47839087+Sherman0236@users.noreply.github.com>
2020-04-25 07:02:45 -0400

    Basic Sanitization Checks

--- Soltero/Core/dllmain.cpp
+++ Soltero/Core/dllmain.cpp
@@ -4,7 +4,19 @@
 void Main()
 {
 	Global::Modules.Mono = GetModuleHandleA("mono-2.0-bdwgc.dll");																						// Get a handle to the mono module
+	if (!Global::Modules.Mono)
+	{
+		printf("Mono module not loaded!\n");
+		return;
+	}
+
 	Global::Functions.MonoImageLoaded = reinterpret_cast<uint64_t(__fastcall *)(char*)>(GetProcAddress(Global::Modules.Mono, "mono_image_loaded"));		// Get the address of the function 'mono_image_loaded'
+	if (!Global::Functions.MonoImageLoaded)
+	{
+		printf("Mono_Image_Loaded not found!\n");
+		return;
+	}
+
 	printf("Address\tSize\tName\n");
 	uint64_t pImage = Global::Functions.MonoImageLoaded((char*)"Assembly-CSharp");																		// Call the function for Assembly-CSharp and store the return
 	printf("%I64X\t%I32X\t%s\n", pImage, *reinterpret_cast<uint32_t*>(pImage + 0x18), pImage);															
